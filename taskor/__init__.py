36616fd1118a5e1d3925ad4d5b34d0ad5374d7ab
l0g1h <redfisher69@protonmail.com>
2024-12-22 13:18:51 +0200

    if that doesnt work im eating the dog

--- taskor/__init__.py
+++ taskor/__init__.py
@@ -1,1 +1,4 @@
 from .taskor import main
+from .utils.file_extractor import extract_text_from_file
+from .utils.assistant_api import get_completion
+from .utils.file_selector import get_file_paths
--- taskor/utils/__init__.py
+++ taskor/utils/__init__.py
@@ -1,3 +1,3 @@
+from .file_extractor import extract_text_from_file
 from .assistant_api import get_completion
-from .file_extractor import extract_text_from_image
 from .file_selector import get_file_paths
